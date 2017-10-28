f50fb6517493da7fe8cf719c8b32dcb4ca2d4fbe
Unknown <valentin.gushan@gmail.com>
2017-10-28 12:18:38 +0300

    moved to src

--- src/ApinationGateway/Program.cs
+++ src/ApinationGateway/Program.cs
@@ -10,7 +10,7 @@ namespace ApinationGateway
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application
         /// </summary>
         static void Main()
         {
