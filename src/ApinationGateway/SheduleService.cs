c2a237fd285d9dcbaa925c7778f666125995260d
Unknown <valentin.gushan@gmail.com>
2017-10-28 22:44:58 +0300

    log4net research

--- src/ApinationGateway/SheduleService.cs
+++ src/ApinationGateway/SheduleService.cs
@@ -34,7 +34,7 @@ namespace ApinationGateway
 
         protected override void OnStart(string[] args)
         {
-            Log.Info("OnStart");
+            log.Info("OnStart");
         }
 
         protected override void OnStop()
