b782c4ed805a4f7e527c54dbd479707c8b576fd2
valentin.gushan <valentin.gushan@gmail.com>
2017-10-31 08:22:35 +0200

    Refactorings reame API to Repository added schemas stubs comments

---
+++ src/Sage50Connector/Core/ApplicationConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+
+namespace Sage50Connector.Core
+{
+    static class ApplicationConfig
+    {
+        /// <summary>
+        /// A valid application ID is needed in order to connect to regular Sage 50 companies
+        /// </summary>
+        public static string Sage50ApplicationID => ConfigurationManager.AppSettings["Sage50ApplicationID"];
+
+        /// <summary>
+        /// Authenticate token for authorization purpose
+        /// </summary>
+        public static string AuthToken => ConfigurationManager.AppSettings["AuthToken"];
+
+        /// <summary>
+        /// Apination REST Api Base Url
+        /// </summary>
+        public static Uri ApinationBaseUri => new Uri(ConfigurationManager.AppSettings["ApinationBaseUrl"]);
+    }
+}
