d4ef73accce79d0617678b22061edac26aacc3d3
Serghei Belyi <sergey.belyi@gmail.com>
2017-11-25 16:39:23 +0200

    MainAddress

--- src/Sage50Connector.Models/Data/Payment.cs
+++ src/Sage50Connector.Models/Data/Payment.cs
@@ -6,6 +6,7 @@ namespace Sage50Connector.Models.Data
     {
         public Vendor Vendor { get; set; }
         public Account DiscountAccount { get; set; }
+        public NameAndAddress MainAddress { get; set; }
         public List<PaymentInvoiceLine> ApplyToInvoiceLines { get; set; }
         public List<PaymentExpenseLine> ApplyToExpenseLines { get; set; }
         public string Memo { get; set; }
